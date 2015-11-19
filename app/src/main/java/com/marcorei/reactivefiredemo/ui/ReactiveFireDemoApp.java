cc38294c48d79a2b093686bfa38a64b1684a8b62
Markus Riegel <riegel.markus@googlemail.com>
2015-11-19 18:31:54 +0100

    (add) project files

---
+++ app/src/main/java/com/marcorei/reactivefiredemo/ui/ReactiveFireDemoApp.java
@@ -0,0 +1,10 @@
+/* (c) 2015 Markus Riegel
+ * license: MIT
+ */
+package com.marcorei.reactivefiredemo.ui;
+
+/**
+ * TODO: add class description
+ */
+public class ReactiveFireDemoApp {
+}
---
+++ app/src/main/java/com/marcorei/reactivefiredemo/ui/activity/MainActivity.java
@@ -0,0 +1,18 @@
+/* (c) 2015 Markus Riegel
+ * license: MIT
+ */
+package com.marcorei.reactivefiredemo.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.marcorei.reactivefiredemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
